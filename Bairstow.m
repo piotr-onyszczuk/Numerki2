7ed7d163a35117383ee2bd9c8233a3e7e71f6b27
Maciej Pióro <maciej.pioro@gmail.com>
2018-10-06 22:13:11 +0200

    Trochę ładniej, wyznacza wszystkie miejsca zerowe

--- Bairstow.m
+++ Bairstow.m
@@ -1,26 +1,27 @@
-function x0 = Bairstow(coef, x0)
-%Bairwsow - Description
+function [res] = Bairstow(poly, x0, eps, tol, iter)
+% BAIRSTOW finds all roots of a polynomial using Bairstow's method
 %
 % Syntax: function = Bairstow(coef, x0)
 %
 % Long description
-    x0 = x0(:);
-    coef = coef(:);
+x0 = x0(:);
+poly = poly(:);
+res=[];
+while(length(poly)>3)
     for i=1:100
-        p=[1; -x0(:)];
-        [p1, AB] = deconv(coef, p);
-        AB=AB(end-1:end);
-        AB=AB(:)
-        [~,AB1]=deconv(p1, p);
+        quadratic=[1; -x0(:)];
+        [poly1, remainder] = deconv(poly, quadratic);
+        remainder=remainder(end-1:end);
+        [~,AB1]=deconv(poly1, quadratic);
         AB1=AB1(end-1:end);
-        AB1=AB1(:);
-        %J=[Ar, Aq; Br, Bq];
         A1=AB1(1);
         B1=AB1(2);
-        Aq=A1;
-        Bq=B1;
         Ar=x0(1)*A1+B1;
         Br=x0(2)*A1;
-        x0=x0-inv([Ar, Aq; Br, Bq])*AB;
+        x0=x0-[Ar, A1; Br, B1]\remainder;
     end
+    res=[roots([1; -x0(:)]);res];
+    poly=deconv(poly, quadratic);
+end
+res = [roots(poly);res];
 end

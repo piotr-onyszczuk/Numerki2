e79f0303ccd3f79721628aaeb21684f377335b0d
Maciej Pióro <maciej.pioro@gmail.com>
2018-10-13 15:42:41 +0200

    niw ie

--- Bairstow.m
+++ Bairstow.m
@@ -17,13 +17,12 @@ x0 = x0(:);
 poly = poly(:);
 res=[];
 quadratic=[1; -x0];
-root=roots(quadratic);
 convergent=true;
 while length(poly)>3 && convergent
     i=1;
-    while polyval(poly,root(1))>eps &&  i<=iter
-        [poly1, remainder] = deconv(poly, quadratic);
-        remainder=remainder(end-1:end); % ostatnie dwa elementy reszty
+    [poly1, remainder] = deconv(poly, quadratic);
+    remainder=remainder(end-1:end); % ostatnie dwa elementy reszty
+    while norm(remainder)>eps &&  i<=iter
         [~,AB1]=deconv(poly1, quadratic);
         AB1=AB1(end-1:end);
         A1=AB1(1);
@@ -35,12 +34,14 @@ while length(poly)>3 && convergent
         if isequal(warn, 'MATLAB:singularMatrix')
             % W przypadku wystąpienia osobliwej macierzy Jacobiego
             % symulujemy przekroczenie dozwolonej ilosci iteracji
+            fprintf("singular")
             i=iter+1;
             lastwarn('');
         end
         quadratic=[1; -x0];
+        [poly1, remainder] = deconv(poly, quadratic);
+        remainder=remainder(end-1:end); % ostatnie dwa elementy reszty
         i=i+1;
-        root=roots(quadratic);
     end
     if i>iter 
         % W przypadku niepowodzenia metody Bairstowa używamy metody
@@ -50,31 +51,30 @@ while length(poly)>3 && convergent
             % jezeli bisekcja zwróciła poprawny wynik, dodajemy go do
             % wektora wynikowego. W przeciwnym przypadku zakończymy
             % działanie programu
+            fprintf("bisekcaj")
             res=[res; bisectionroot];
-            while polyval(poly, bisectionroot)<eps
-                % eliminujemy zera wielokrotne wielomianu poly
-                poly=deconv(poly, [1 ; -bisectionroot]);
-            end
+            poly=deconv(poly, [1 -bisectionroot]);
+%             while polyval(poly, bisectionroot)<eps
+%                 % eliminujemy zera wielokrotne wielomianu poly
+%                 poly=deconv(poly, [1 ; -bisectionroot]);
+%             end
         end
     else
         % za pomocą roots znajdujemy miejsca zerowe wielomianu kwadratowego
         % znalezionego metodą Bairstowa
-        quadraticroots=roots([1; -x0]);
+        quadraticroots=roots(quadratic);
         res=[quadraticroots; res];
-        poly=deconv(poly, quadratic);
-        while polyval(poly, quadraticroots(1))<eps
-            % eliminujemy zera wielokrotne wielomianu poly
-            poly=deconv(poly, [1 ; -quadraticroots(1)]);
-        end
-        while polyval(poly, quadraticroots(2))<eps
-            % eliminujemy zera wielokrotne wielomianu poly
-            poly=deconv(poly, [1 ; -quadraticroots(2)]);
-        end
+        [poly, remainder]=deconv(poly, quadratic);
+%         remainder=remainder(end-1:end)
+%         norm(remainder)
+%         while(norm(remainder)<eps)
+%             [poly, remainder]=deconv(poly, quadratic);
+%         end
     end
 end
 if ~convergent
     fprintf("Nie znaleziono wszystkich pierwiastków wielomianu");
 end
 if length(poly)<=3
-    res=[roots(poly); res]
+    res=[roots(poly); res];
 end

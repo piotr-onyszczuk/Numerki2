1f1a60855319d9bd847c9cf345706a29f8850d2e
unknown <piotr.onyszczuk1@gmail.com>
2018-10-13 13:48:23 +0200

    repair bisection method

--- bisekcja.m
+++ bisekcja.m
@@ -1,37 +1,58 @@
 function [convergent, tmpzerowe] = bisekcja(poly)
-A=linspace(-1e4,1e4);
-negative=0;
-positive=0;
+
 convergent=true;
-for i=1:length(A)
-    if(polyval(poly,A(i))==0)
-        convergent=true;
-        tmpzerowe=A(i);
-        return;
+%pierwsze przyblizenie
+x0=-poly(2)/poly(1); 
+%stopien nieparzysty
+if (mod(length(poly),2)==0) 
+    %miejsce zerowe jest na lewo od x0
+    if polyval(poly,x0)*poly(1)>0 
+        right=x0;
+        left=x0-10;
+        %szukamy miejsca z przeciwnym znakiem
+        while polyval(poly,left)*poly(1)>0 
+            left=left-10;
+        end
+    %miejsce zerowe jest na prawo od x0
+    else 
+        left=x0;
+        right=x0+10;
+        while polyval(poly,right)*poly(1)<0
+            right=right+10;
+        end
     end
-    if(polyval(poly,A(i))<0)
-        negative=A(i);
-    else
-        positive=A(i);
+
+%stopien parzysty
+else
+    %zaczynamy poszukiwania na lewo od x0
+    left=x0-100; 
+    %bedziemy przesuwac sie o h
+    h=0.5; 
+    right=left+h;
+    while polyval(poly,left)*polyval(poly,right)>0 && left<x0+100
+        left=left+h;
+        right=right+h;
     end
 end
-if polyval(poly,positive)<0 || polyval(poly,negative)>0
-    convergent=false;
-    return;
+
+%sprawdzenie czy znalezlismy odpowiedni przedzial
+if polyval(poly,left)*polyval(poly,right)>0 
+    convergent = false;
+    return
 end
 
 %bisekcja
 for i=1:1000
-    middle=(negative+positive)/2;
+    middle=(left+right)/2;
     if (polyval(poly,middle))==0
         tmpzerowe=middle;
         return;
     end
-    if (polyval(poly,middle))>0
-        positive=middle;
+    if polyval(poly,middle)*polyval(poly,right)>0
+        right=middle;
     else
-        negative=middle;
+        left=middle;
     end
 end
-tmpzerowe=(negative+positive)/2;
+tmpzerowe=(left+right)/2;
 end

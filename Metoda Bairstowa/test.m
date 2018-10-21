164d5da6b40891ce4849ac14161e80f1744d3123
Maciej Pióro <maciej.pioro@gmail.com>
2018-10-21 22:48:29 +0200

    get complex

--- Metoda Bairstowa/test.m
+++ Metoda Bairstowa/test.m
@@ -5,7 +5,7 @@ max_root=10;
 results=zeros(max_root-min_root+1, 2);
 for i=min_root:max_root
     p=poly(1:i);
-    results(i-min_root+1, 1)=sum(abs(polyval(p, Bairstow(p, x0, 1e-9, [], [], []))))/i;
+    results(i-min_root+1, 1)=sum(abs(polyval(p, Bairstow(p, x0, [], [], [], []))))/i;
     results(i-min_root+1, 2)=sum(abs(polyval(p, roots(p))))/i;
 end
 figure
@@ -15,6 +15,7 @@ legend('Bairstow', 'roots')
 %Time needed
 for i=min_root:max_root
     p=poly(1:i);
+    p;
     f=@() Bairstow(p, x0, [], [], [], []);
     results(i-min_root+1, 1)=timeit(f);
     f=@() roots(p);
@@ -24,3 +25,30 @@ figure
 semilogy(min_root:max_root, results(:, 1), min_root:max_root, results(:, 2))
 title('time')
 legend('Bairstow', 'roots')
+%And now for something completely complex
+%LARCH
+min_root=4;
+max_root=16;
+results=zeros(max_root-min_root+1, 2);
+for i=min_root:max_root
+    p=[1 zeros(1, i-1) -1];
+    results(i-min_root+1, 1)=sum(abs(polyval(p, Bairstow(p, x0, [], [], [], []))))/i;
+    results(i-min_root+1, 2)=sum(abs(polyval(p, roots(p))))/i;
+end
+figure
+semilogy(min_root:max_root, results(:, 1), min_root:max_root, results(:, 2))
+title('poly error')
+legend('Bairstow', 'roots')
+%Time needed
+for i=min_root:max_root
+    p=[1 zeros(1, i-1) -1];
+    f=@() Bairstow(p, x0, [], [], [], []);
+    results(i-min_root+1, 1)=timeit(f);
+    f=@() roots(p);
+    results(i-min_root+1, 2)=timeit(f);
+end
+figure
+semilogy(min_root:max_root, results(:, 1), min_root:max_root, results(:, 2))
+title('time')
+legend('Bairstow', 'roots')
+

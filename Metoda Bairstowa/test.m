650b269d2c707767c48dcd35d8a72f7ef6b40e54
Maciej Pióro <maciej.pioro@gmail.com>
2018-10-23 21:12:36 +0200

    całkiem git

--- Metoda Bairstowa/test.m
+++ Metoda Bairstowa/test.m
@@ -1,17 +1,22 @@
 %Error of the method
 x0=[0, 0];
-min_root=4;
+min_root=3;
 max_root=10;
 results=zeros(max_root-min_root+1, 2);
+
 for i=min_root:max_root
     p=poly(1:i);
     results(i-min_root+1, 1)=sum(abs(polyval(p, Bairstow(p, x0, [], [], [], []))))/i;
     results(i-min_root+1, 2)=sum(abs(polyval(p, roots(p))))/i;
 end
-figure
+subplot(2, 2, 1)
 semilogy(min_root:max_root, results(:, 1), min_root:max_root, results(:, 2))
-title('poly error')
+title('Średni błąd (poly(1:n))')
+ylabel('Średni błąd')
+xlabel('Stopień wielomianu (n)')
 legend('Bairstow', 'roots')
+axis([min_root max_root -inf inf])
+axis('auto y')
 %Time needed
 for i=min_root:max_root
     p=poly(1:i);
@@ -21,13 +26,17 @@ for i=min_root:max_root
     f=@() roots(p);
     results(i-min_root+1, 2)=timeit(f);
 end
-figure
+subplot(2, 2, 2)
 semilogy(min_root:max_root, results(:, 1), min_root:max_root, results(:, 2))
-title('time')
+title('Czas wykonania (poly(1:n))')
+ylabel('Czas [s]')
+xlabel('Stopień wielomianu (n)')
 legend('Bairstow', 'roots')
+axis([min_root max_root -inf inf])
+axis('auto y')
 %And now for something completely complex
 %LARCH
-min_root=4;
+min_root=3;
 max_root=16;
 results=zeros(max_root-min_root+1, 2);
 for i=min_root:max_root
@@ -35,10 +44,14 @@ for i=min_root:max_root
     results(i-min_root+1, 1)=sum(abs(polyval(p, Bairstow(p, x0, [], [], [], []))))/i;
     results(i-min_root+1, 2)=sum(abs(polyval(p, roots(p))))/i;
 end
-figure
+subplot(2, 2, 3)
 semilogy(min_root:max_root, results(:, 1), min_root:max_root, results(:, 2))
-title('poly error')
+title('Średni błąd (x^n-1)')
+ylabel('Średni błąd')
+xlabel('Stopień wielomianu (n)')
 legend('Bairstow', 'roots')
+axis([min_root max_root -inf inf])
+axis('auto y')
 %Time needed
 for i=min_root:max_root
     p=[1 zeros(1, i-1) -1];
@@ -47,8 +60,13 @@ for i=min_root:max_root
     f=@() roots(p);
     results(i-min_root+1, 2)=timeit(f);
 end
-figure
+subplot(2, 2, 4)
 semilogy(min_root:max_root, results(:, 1), min_root:max_root, results(:, 2))
-title('time')
+title('Czas wykonania (x^n-1)')
+ylabel('Czas [s]')
+xlabel('Stopień wielomianu (n)')
 legend('Bairstow', 'roots')
+axis([min_root max_root -inf inf])
+axis('auto y')
+
 

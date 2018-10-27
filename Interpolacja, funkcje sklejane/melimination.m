bedd659878450e605807cdf87cff871189ec4846
Maciej Pióro <maciej.pioro@gmail.com>
2018-10-27 14:58:38 +0200

    make it really fast

--- Interpolacja, funkcje sklejane/melimination.m
+++ Interpolacja, funkcje sklejane/melimination.m
@@ -3,10 +3,14 @@ function [x] = melimination(A, b)
 %   Detailed explanation goes here
 Ab=[A b];
 n=size(A, 1);
-for i=1:n
+Ab(1, :)=Ab(1, :)/Ab(1, 1);
+Ab(2, :)=Ab(2, :)-Ab(1, :).*Ab(2,1);
+for i=2:n-1
     Ab(i, i:end)=Ab(i, i:end)/Ab(i, i);
     Ab(1:i-1, i:end)=Ab(1:i-1, i:end)-Ab(i, i:end).*Ab(1:i-1, i);
-    Ab(i+1:end, i:end)=Ab(i+1:end, i:end)-Ab(i, i:end).*Ab(i+1:end, i);
+    Ab(i+1, i:end)=Ab(i+1, i:end)-Ab(i, i:end).*Ab(i+1, i);
 end
+Ab(n, n:end)=Ab(n, n:end)/Ab(n, n);
+Ab(1:n-1, n:end)=Ab(1:n-1, n:end)-Ab(n, n:end).*Ab(1:n-1, n);
 x=Ab(:, end);
 end

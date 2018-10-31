266e6ceb8d960eb60939224036a33792283b7606
unknown <piotr.onyszczuk1@gmail.com>
2018-10-31 13:42:16 +0100

    show example

--- Interpolacja, funkcje sklejane/test.m
+++ Interpolacja, funkcje sklejane/test.m
@@ -1,11 +1,11 @@
 %test
 %badanie metody interpolacji funkcjami sklejanymi kubicznymi dla funkcji
 %fun na przedziale [a,b]
-a=2;
-b=10;
-fun=@(x)sin(x)+5;
+a=-6;
+b=6;
+fun=@(x)1./(1+x.^2);
 %Jedno nalezy zakomentowac:
-der2=@(x)-sin(x); %1: pochodna wyznaczona recznie, analitycznie
+der2=@(x)(6.*x.^2-2)./((1+x.^2).^3); %1: pochodna wyznaczona recznie, analitycznie
 %der1=@(x)derivative(fun, x);  %2: pochodna liczona numerycznie
 %der2=@(x)derivative(der1, x); %   numeryczna 2. pochodna
 %wartosc pochodnej w a i b

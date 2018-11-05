840a197eb0f34a53491beb9b75c0242c41df08e5
Maciej Pióro <maciej.pioro@gmail.com>
2018-11-05 22:47:44 +0100

    test z e(x^2(

--- Interpolacja, funkcje sklejane/test.m
+++ Interpolacja, funkcje sklejane/test.m
@@ -1,11 +1,11 @@
 %test
 %badanie metody interpolacji funkcjami sklejanymi kubicznymi dla funkcji
 %fun na przedziale [a,b]
-a=-60;
-b=60;
-fun=@(x)1./(1+x.^2);
+a=9;
+b=10;
+fun=@(x)exp(x.^2);
 %Jedno nalezy zakomentowac:
-der2=@(x)(6.*x.^2-2)./((1+x.^2).^3); %1: pochodna wyznaczona recznie, analitycznie
+der2=@(x)2*exp(x.^2).*(2*x.^2+1); %1: pochodna wyznaczona recznie, analitycznie
 %der1=@(x)derivative(fun, x);  %2: pochodna liczona numerycznie
 %der2=@(x)derivative(der1, x); %   numeryczna 2. pochodna
 %wartosc pochodnej w a i b

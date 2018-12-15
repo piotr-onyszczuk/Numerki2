03cbc68ff02f093a0a9839d6f6eec232bd6e3810
unknown <piotr.onyszczuk1@gmail.com>
2018-12-15 13:39:50 +0100

    correct Scraton

--- Metoda Scratona/Scraton.m
+++ Metoda Scratona/Scraton.m
@@ -1,31 +1,29 @@
-function [] = Scraton(a,b,n,ya,f,y)
-%funkcja rozwiazuje zagadnienie poczatkowe y'=f(x,y), fa=y(a) podajac 
-%wartosci funkcji w n+1 rownoodleglych punktach na przedziale [a,b]
+function [] = Scraton(x,ya,f,y)
+%funkcja rozwiazuje zagadnienie poczatkowe y'=f(x,y), ya=y(x(1)) podajac 
+%wartosci funkcji w argumentach z wektora x
 %
 %Parametry;
-%   a, b - konce przedzialu
-%   n - liczba punktow dla ktorych liczona bedzie wartosc funkcji y
-%   ya - zagadnienie poczatkowe y(a)
+%   x - wektor kolejnych argumentow dla ktorych liczone beda wartosci
+%   funkcji y
+%   ya - zagadnienie poczatkowe y(x(1))
 %   f - prawa strona rownania (patrz opis)
 %   y - funkcja bedaca rozwiazaniem rownania, sluzy do sprawdzania
 %   poprawnosci wynikow (mozna nie podawac)
 
-x=zeros(1,n+1);
+
+n=length(x)-1;
 values=zeros(1,n+1);
-x(1)=a;
 values(1)=ya;
-h=(b-a)/n;
 
 for i=1:n
-   x(i+1)=x(i)+h;
    %obliczenie wartosci w kolejnym punkcie
-   [k1,k3,k4,k5]=Scraton_step(x(i),values(i),h,f);
+   [k1,k3,k4,k5]=Scraton_step(x(i),values(i),x(i+1)-x(i),f);
    values(i+1)=values(i)+(1445*k1+6561*k3+3264*k4+2500*k5)/13770;
 end
 
 
 %rysowanie wykresu
-if (isempty(y) || nargin==5)
+if (isempty(y) || nargin==3)
     plot(x,values,'.');
     legend('wynik metody');
     xlabel('x');
--- Metoda Scratona/Scraton_step.m
+++ Metoda Scratona/Scraton_step.m
@@ -1,14 +1,14 @@
-function [k1,k3,k4,k5]=Scraton_step(xi,yi,h,f)
+function [k1,k3,k4,k5]=Scraton_step(xi,yi,hi,f)
 %funkcja obliczajaca wartosc funkcji bedacej rozwiazaniem rownania
 %rozniczkowego w x(i+1) znajac (x(i),y(i)) metoda Scratona
 % 
 %Parametry: 
 %   xi,yi - obliczone wczesniej wspolrzedne poprzedniego punktu
-%   h - odleglosc na OX pomiedzy kolejnymi punktami
+%   hi - odleglosc na OX pomiedzy x(i+1) oraz x(i)
 %   f - funkcja bedaca prawa strona rownania rozniczkowego
-k1=h*f(xi,yi);
-k2=h*f(xi+2*h/9,yi+2*k1/9);
-k3=h*f(xi+h/3,yi+(k1+3*k2)/12);
-k4=h*f(xi+3*h/4,yi+3*(23*k1-81*k2+90*k3)/128);
-k5=h*f(xi+9*h/10,yi+9*(-345*k1+2025*k2-1224*k3+544*k4)/10000);
+k1=hi*f(xi,yi);
+k2=hi*f(xi+2*hi/9,yi+2*k1/9);
+k3=hi*f(xi+hi/3,yi+(k1+3*k2)/12);
+k4=hi*f(xi+3*hi/4,yi+3*(23*k1-81*k2+90*k3)/128);
+k5=hi*f(xi+9*hi/10,yi+9*(-345*k1+2025*k2-1224*k3+544*k4)/10000);
 end
